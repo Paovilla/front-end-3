0e4be062f2cae6a30702ccc0b6d7c3714363043f
Paola Villa <yuryvilla18@hotmail.com>
2022-08-28 18:35:44 -0500

    Shopping cart creation

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@
 // ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
 // MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import { useState, useEffect } from "react";
+import Cabecera from '../src/components/Cabecera';
+import Listado from '../src/components/Listado';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(0);
+  }, []);
+  
+
+  function handleCount() {
+    const countUpdated = count + 1;
+    setCount(countUpdated)
+  }
 
   return (
     <div className="App">
-      <Cabecera />
-      <Listado />
+      <Cabecera count={count} />
+      <Listado handleCount={handleCount} />
     </div>
   );
 }
--- src/components/Item.js
+++ src/components/Item.js
@@ -9,11 +9,36 @@
 //    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
 //    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
 
-export default function Item() {
+import { useEffect, useState } from "react";
+import '../index.css';
+
+export default function Item(props) {
+  const [stock, setStock] = useState(props.product.stock)
+  const buttonName = stock === 0 ? 'SIN STOCK' : 'COMPRAR';
+  const shouldShowStock = stock === 0 ? false : true;
+  const shouldDisabledButton = stock === 0 ? true : false;
+
+  useEffect(() => {
+    const initialStock = props.product.stock
+    setStock(initialStock);
+  }, [props.product.stock]);
+
+  function handleOnClick() {
+    const stockUpdated = stock - 1;
+    if (stockUpdated >= 0) {
+      props.handleCount();
+      setStock(stockUpdated)
+    } else {
+      setStock(0)
+    }
+  }
 
   return (
     <div className='producto'>
-      {/* maquetar Item aquí */}
+      <h3 className="header">{props.product.producto.nombre}</h3>
+      <p>{props.product.producto.descripcion}</p>
+      <h5>En stock: {shouldShowStock ? stock : <span>agotado</span> }</h5>
+      <button onClick={handleOnClick} disabled={shouldDisabledButton}>{buttonName}</button>
     </div>
   )
 }
